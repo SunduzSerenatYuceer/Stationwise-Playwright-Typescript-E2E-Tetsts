e3fbd44dfa45e282c6ee8392063765b2ce6cc9c2
tolga atis <sunduz.yuceer@gmail.com>
2024-11-11 00:00:54 +0300

    Adding Prefences Page Scenarios

---
+++ e2e/pages/prefences-page/page.ts
@@ -0,0 +1,19 @@
+import { Page as BasePage, TestInfo } from "@playwright/test";
+import  { CorePage } from "../../../src/page/CorePage";
+import { PrefencesAction } from "./action";
+import { PrefencesAssertion } from "./assertion";
+
+export class PrefencesPage extends CorePage {
+
+    public action : PrefencesAction;
+    public assertion : PrefencesAssertion;
+
+    constructor(page : BasePage, testInfo: TestInfo) {
+
+        super(page, testInfo);
+        this.action = new PrefencesAction(page, testInfo);
+        this.assertion = new PrefencesAssertion(page, testInfo);
+
+    }
+
+}
